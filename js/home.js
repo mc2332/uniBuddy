fc3c6afffadd467be83819e0c933ddcf9d3b3c2b
rmw62 <60975433+rmw62@users.noreply.github.com>
2020-04-05 22:17:24 +0100

    aesthetic

--- js/home.js
+++ js/home.js
@@ -1,14 +1,22 @@
 const happyPhrases = [
     "Please try harder",
     "Everyone is relying on you",
-    "This is the most important time of your life"
+    "This is the most important time of your life",
+    "Do a flip"
 ];
+
+const backgroundCount = 4;
 
 function pickRandom(array) {
     const arrayLength = array.length;
     const randInt = Math.floor(Math.random() * arrayLength);
     return array[randInt];
-};
+}
+
+function displayBackground() {
+    const randInt = Math.floor(Math.random() * backgroundCount);
+    document.body.style.backgroundImage=`url("./assets/tree${randInt}.jpg")`;
+}
 
 function displayHappyPhrase(happyMessageId) {
     const happyMessage = document.getElementById(happyMessageId);
@@ -18,4 +26,5 @@ function displayHappyPhrase(happyMessageId) {
 
 function init(happyMessageId) {
     displayHappyPhrase(happyMessageId);
+    displayBackground();
 }
