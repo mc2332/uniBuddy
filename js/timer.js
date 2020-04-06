50ee55f1162a2160f78d7c18deb422141dd7b6a8
rmw62 <60975433+rmw62@users.noreply.github.com>
2020-04-06 15:53:02 +0100

    timer stuff

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,4 @@
-const backgroundCount = 4;
-
-function displayBackground() {
+function displayBackgroundTimer() {
     const randInt = Math.floor(Math.random() * backgroundCount);
     document.body.style.backgroundImage=`url("../assets/tree${randInt}.jpg")`;
 }
